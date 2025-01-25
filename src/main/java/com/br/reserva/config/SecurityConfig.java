eee5234dfb9993528a4dcc641c8ac1181400e10d
Thiago Vieira <thiagomlu@hotmail.com.br>
2025-01-25 13:15:38 -0300

    feat: config for token // todo::fix authenticate token for update and delete

--- src/main/java/com/br/reserva/config/SecurityConfig.java
+++ src/main/java/com/br/reserva/config/SecurityConfig.java
@@ -1,5 +1,6 @@
     package com.br.reserva.config;
 
+    import com.br.reserva.security.JwtAuthenticationFilter;
     import org.springframework.context.annotation.Bean;
     import org.springframework.context.annotation.Configuration;
     import org.springframework.security.config.Customizer;
@@ -8,6 +9,7 @@
     import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
     import org.springframework.security.crypto.password.PasswordEncoder;
     import org.springframework.security.web.SecurityFilterChain;
+    import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
     @Configuration
     public class SecurityConfig {
@@ -18,13 +20,17 @@
         }
 
         @Bean
-        public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-            httpSecurity
-                    .authorizeHttpRequests(auth -> auth.anyRequest().permitAll()
+        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+            http
+                    .csrf(csrf -> csrf.disable()) // Desativa CSRF para APIs REST
+                    .authorizeHttpRequests(auth -> auth
+                            .requestMatchers("/api/users/login").permitAll() // Permite acesso público ao login
+                            .requestMatchers("/api/users").permitAll() // Permite criação de usuário
+                            .anyRequest().authenticated() // Protege todos os outros endpoints
                     )
-                    .httpBasic(Customizer.withDefaults())
-                    .csrf(AbstractHttpConfigurer::disable);
-            return httpSecurity.build();
+                    .addFilterBefore(new JwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class); // Filtro JWT
+
+            return http.build();
         }
 
     }
